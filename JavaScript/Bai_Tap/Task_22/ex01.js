f4b0f17a1880cb399fa1a6918d3989a67a7d726a
TCSon03 <truongcongsson@gmail.com>
2025-04-05 20:23:46 +0700

    Task 22: --Done

--- JavaScript/Bai_Tap/Task_22/ex01.js
+++ JavaScript/Bai_Tap/Task_22/ex01.js
@@ -1,16 +1,22 @@
-const arrayNumber = [1, 2, 3, 4, 3, 4, 1];
-let boolean = false;
-
-for (let i = 0; i < arrayNumber.length / 2; i++) {
-  //   console.log(arrayNumber[arrayNumber.length - 1 - i]);
-  if (arrayNumber[i] === arrayNumber[arrayNumber.length - 1 - i]) {
-    boolean = true;
-  } else {
-    boolean = false;
-    break;
+let input = prompt("Nhập các số vào mảng, (Ví dụ: 1,2,3,4,3,2,1)");
+let arrayNumber;
+if (input === null || input.trim() === "") {
+  document.write("Cần nhập đúng dữ liệu");
+} else {
+  arrayNumber = input.split(",").map(Number);
+  document.write("Mảng của bạn: " + arrayNumber);
+  function checkSymmetricalArr(arr) {
+    // Xử lý logic
+    if (arr.length === 1) {
+      return "Mảng có 1 phần tử, không kiểm tra được.";
+    }
+    for (let s = 0; s < arr.length / 2; s++) {
+      if (arr[s] !== arr[arr.length - 1 - s]) {
+        return false;
+      }
+    }
+    return true;
   }
+  const result = checkSymmetricalArr(arrayNumber);
+  document.write("<br>" + "Mảng của bạn: " + result + " đối xứng");
 }
-console.log(boolean);
-
-// const result = checkSymmetricalArr(arrayNumber);
-// console.log(result);
