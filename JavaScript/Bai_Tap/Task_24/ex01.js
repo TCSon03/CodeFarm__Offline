deba50f88a8297e3804127dd151c01813696946a
TCSon03 <truongcongsson@gmail.com>
2025-04-08 10:23:33 +0700

    task 24

--- JavaScript/Bai_Tap/Task_24/ex01.js
+++ JavaScript/Bai_Tap/Task_24/ex01.js
@@ -1,9 +1,19 @@
 const number = [4, 2, 7, 6, 5, 8, 2, 1];
+document.writeln("Mảng ban đầu: " + number + "<br>")
 function sortArrNumber(arr) {
-  const newNumber = []
+  // Xử lý và in ra kết quả
   for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
+    // console.log(arr[i]);
+    for (let j = arr.length - 1; j > i; j--) {
+      if (arr[j] < arr[j - 1]) {
+        let t = arr[j];
+        arr[j] = arr[j - 1];
+        arr[j - 1] = t;
+      }
+    }
   }
+  return arr;
 }
+
 const result = sortArrNumber(number);
-// console.log(result);
+document.writeln("Mảng sau khi sắp xếp: " + result); // [1,2,2,4,5,6,7,8]
