78af7b2e2da150485a38f24bd69e18d648ede630
TCSon03 <truongcongsson@gmail.com>
2025-04-09 13:55:18 +0700

    phuong thuc

--- JavaScript/Bai_Tap/Task_26/ex01.js
+++ JavaScript/Bai_Tap/Task_26/ex01.js
@@ -15,48 +15,49 @@
 //   NaN,
 // ];
 
-// function removeDuplicate(arr) {
-//   if (!Array.isArray(arr)) {
-//     return;
-//   }
-//   const initialValue = {};
-//   const sumWithInitial = arr.reduce((accumulator, currentValue) => {
-//     if (!accumulator.includes(currentValue)) {
-//       return {...accumulator, currentValue};
-//     }
-//     return accumulator;
-//   }, initialValue);
-//   return sumWithInitial;
-// }
-// const result = removeDuplicate(fruits);
-// console.log(result);
-
-const fruits = [
-  "apple",
-  "banana",
-  "kiwi",
-  "kiwi",
-  "banana",
-  "orange",
-  "apple",
-  "kiwi",
-];
-
-function countElement(arr) {
-  // Xử lý và in ra kết quả
-  const newObject = {};
-
-  for (let i = 0; i < arr.length; i++) {
-    const fruit = arr[i];
-    if (newObject[fruit]) {
-      newObject[fruit]++;
-    } else {
-      newObject[fruit] = 1;
-    }
+function removeDuplicate(arr) {
+  if (!Array.isArray(arr)) {
+    return;
   }
-  return newObject;
+  const initialValue = {};
+  const sumWithInitial = arr.reduce((accumulator, currentValue) => {
+    console.log("accumulator", accumulator);
+
+    if (!accumulator.includes(currentValue)) {
+      return { ...accumulator, currentValue };
+    }
+    return accumulator;
+  }, initialValue);
+  return sumWithInitial;
 }
+const result = removeDuplicate(fruits);
+console.log(result);
 
-const result = countElement(fruits);
+// const fruits = [
+//   "apple",
+//   "banana",
+//   "kiwi",
+//   "kiwi",
+//   "banana",
+//   "orange",
+//   "apple",
+//   "kiwi",
+// ];
 
-console.log(result); // { apple: 2, banana: 2, kiwi: 3, orange: 1 }
+// function countElement(arr) {
+//   // Xử lý và in ra kết quả
+//   const newObject = {};
+
+//   for (let i = 0; i < arr.length; i++) {
+//     const fruit = arr[i];
+//     if (newObject[fruit]) {
+//       newObject[fruit]++;
+//     } else {
+//       newObject[fruit] = 1;
+//     }
+//   }
+//   return newObject;
+// }
+
+// const result = countElement(fruits);
+// console.log(result); // { apple: 2, banana: 2, kiwi: 3, orange: 1 }
